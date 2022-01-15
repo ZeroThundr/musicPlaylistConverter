48b0e83ab859263e9f4451b286450816d0f48627
ZeroThundr <bryceolcott@gmail.com>
2022-01-15 01:17:04 -0700

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
-package spotifyYoutubeConverter
+package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
+//Function for converting spotify playlist to a YouTube playlist
+/*func spotifyToYoutube() {
+
+}
+
+//Function for converting a YouTube playlist to a Spotify playlist
+func youTubeToSpotify() {
+
+}*/
 func main() {
-	fmt.Println("Stop deleting this")
+	//var start int
+	//var finish int
+	//Ask what they are converting from, and assign to start
+	fmt.Println("What are you converting from?")
+	fmt.Println("1. Spotify | 2. YouTube | 3. Apple Music")
+	//ask what they are converting to, and assign to finish
+	fmt.Println("What are you converting to?\n1. Spotify | 2. YouTube | 3. Apple Music")
+
 }
